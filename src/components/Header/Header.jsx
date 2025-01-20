5cbe0511eeeed361c19fd815896e01fa0485ea2a
Hiba <hibakaawan98@gmail.com>
2025-01-19 19:02:27 -0600

    created a basic header with links routing to the appropriate page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Header.scss";
 
 function Header({ user }) {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">HandOver</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-      </ul>
-    </nav>
+    <header className="header">
+      <Link to={"/"} className='header__link'>
+        <h1 className="header__title">
+          HandOver
+        </h1>
+      </Link>
+      <nav className="header__nav">
+        <ul className="header__list">
+          <li className="header__list-item">
+            <Link to={"/"} className='header__nav-link'>Home</Link>
+          </li>
+          <li className="header__list-item">
+            <Link to={"/profile"} className='header__nav-link'>Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    // <nav>
+    //   <ul>
+    //     <li><Link to="/">HandOver</Link></li>
+    //     <li><Link to="/profile">Profile</Link></li>
+    //   </ul>
+    // </nav>
   );
 }
 
