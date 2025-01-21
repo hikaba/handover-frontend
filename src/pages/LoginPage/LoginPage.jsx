16d4734c1d8241d919b3f5816aa82675b5fed0f6
Hiba <hibakaawan98@gmail.com>
2025-01-20 21:24:55 -0600

    removed the login header because page was self-explanatory

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -6,7 +6,6 @@ function LoginPage(){
     return(
         <div className="login-page">
             <h1 className="login-page__header">HandOver</h1>
-            <p className="login-page__text">Login</p>
             <Login />
         </div>
     )
