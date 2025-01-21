6497aeba4f91cd4bdb10ff4eccc8976c44f5a00b
Hiba <hibakaawan98@gmail.com>
2025-01-21 11:57:11 -0600

    added logout button in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { doSignOut } from '../../firebase/auth';
 import "./Header.scss";
 
 function Header({ user }) {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try{
+      await doSignOut();
+      navigate('/login');
+    } catch (error) {
+      console.error("error signing out: ", error.message)
+    }
+  }
+
   return (
     <header className="header">
       <Link to={"/"} className='header__link'>
@@ -17,6 +29,9 @@ function Header({ user }) {
           </li>
           <li className="header__list-item">
             <Link to={"/profile"} className='header__nav-link'>Profile</Link>
+          </li>
+          <li className="header__list-item">
+            <Link onClick={handleLogout} className='header__nav-link header__logout-btn'>Logout</Link>
           </li>
         </ul>
       </nav>
