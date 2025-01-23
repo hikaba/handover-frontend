956ec271d4d540acd028e51db48b7b47592369c4
Hiba <hibakaawan98@gmail.com>
2025-01-22 20:25:33 -0600

    added some styling

--- src/components/PatientCard/PatientCard.jsx
+++ src/components/PatientCard/PatientCard.jsx
@@ -5,8 +5,8 @@ function PatientCard({ patient }) {
     return (
         <Link to={`/patient/${patient.id}`} className="patient">
             <div className="patient-card__container">
-                <p className="patient_name">{patient.last_name}, {patient.first_name}</p>
-                <p className="patient_DOB">Date of Birth: {patient.date_of_birth}</p>
+                <p className="patient__name">{patient.last_name}, {patient.first_name}</p>
+                <p className="patient__DOB">Date of Birth: {patient.date_of_birth}</p>
             </div>
         </Link>
         
--- src/components/PatientListView/PatientListView.jsx
+++ src/components/PatientListView/PatientListView.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PatientCard from "../PatientCard/PatientCard";
+import "./PatientListView.scss";
 
 function PatientListView({ patients }){
     return(
-        <div className="patient-list">
-            <p className="patient_header">Patient List</p>
-            <div className="patient-card__container">
+        <div className="patient__list">
+            <div className="patient-cards__container">
+            <p className="patient_header">Patients</p>
                 {patients.map(patient => (
                     <PatientCard key={patient.id} patient={patient} />
                 ))}
